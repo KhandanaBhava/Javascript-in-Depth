391b6a3878c964cdd430a53efab91d4ec97a8768
Aratrik Mukherjee <aratrikmukherjee2000@gmail.com>
2025-09-26 00:54:23 +0530

    JS Data tpes basics

---
+++ 02_data_types.js
@@ -0,0 +1,24 @@
+// Primitive data types-> number,string,null,undefined,symbol,boolean.
+// Non primitive -> object
+
+let name="aratrik" // string
+
+let age=18 // number 2 to power 53
+
+let isLoggedIn=false // boolean
+
+// null -> stand alone value . Type of null is object.
+
+// undefined -> no value assigned . Type of undefined is undefined( It is a datatype)
+
+// symbol -> uniqueness of components
+
+// object -> non primitive data type
+
+console.log(typeof age)
+
+console.log(typeof false)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
