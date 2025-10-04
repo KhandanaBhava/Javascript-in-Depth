b292bd509a110593b471eb78c25fd2e9c86c899f
Aratrik Mukherjee <aratrikmukherjee2000@gmail.com>
2025-10-04 18:40:59 +0530

    String and Prefix, Postfix operations

---
+++ 04_operations.js
@@ -0,0 +1,70 @@
+//console.log(2**3) ->  2^3=8
+
+//STRRING OPERATIONS
+
+let str1="hello"
+
+let str2=" ram"
+
+//console.log(str1+str2) -> Hello ram
+
+// console.log("1"+2) -> 12 (2 converted to string)
+
+// console.log(1+"2") -> 12 (1 converted to string)
+
+// console.log("1"+"2") -> 12 (both are in string)
+
+// console.log("1"+2+2) -> 122 (Whenver string is not in last , everything is converted to string)
+
+// console.log(1+2+"2") -> 32 (if string is at last, first arithmetic is performed then converted to string)
+
+// console.log(1+"2"+2) -> 122 (Whenver string is not in last , everything is converted to string)
+
+//console.log(+true)  -> 1 ( true converted to number once + is used)
+
+//console.log(true+) -> Error
+
+//console.log(+"") -> 0 (""is false in boolean and is converted to number once + is used)
+
+// PREFIX AND POSTFIX OPERATORS
+
+// let gameCounter=100
+
+// gameCounter++
+
+// console.log(gameCounter) -> 101
+
+// let a=100
+
+// ++a
+
+// console.log(a) -> 101
+
+let x=10
+
+console.log(x++) // -> 10(increment is done after printing value)
+
+console.log(x) // -> 11(incremnet has been done)
+
+let y=10
+
+console.log(++y) // -> 11 (increment is done first, then value is printed)
+
+console.log(y) // -> 11(incremnet has been done)
+
+
+// let m = 3
+
+// const n = m++
+
+// console.log(m); -> 4
+
+// console.log(n); -> 3
+
+// let p=4
+
+// const q=++p
+
+// console.log(p) -> 5
+
+// console.log(q) -> 5
